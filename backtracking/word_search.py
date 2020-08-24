10f75c4cf9ae72960ab17e9df8a04cd2dd29a91d
Josh <josh_hill15@me.com>
2020-08-24 17:43:36 -0400

    completed word search

---
+++ backtracking/word_search.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def exist(self, board, word):
+        visited = {}
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.getWords(board, word, i, j, visited):
+                    return True
+
+        return False
+
+    def getWords(self, board, word, i, j, visited, pos=0):
+        if pos == len(word):
+            return True
+
+        if i < 0 or i == len(board) or j < 0 or j == len(board[0]) or (i, j) in visited or word[pos] != board[i][j]:
+            return False
+
+        visited[(i, j)] = True
+        res = self.getWords(board, word, i, j + 1, visited, pos + 1) \
+            or self.getWords(board, word, i, j - 1, visited, pos + 1) \
+            or self.getWords(board, word, i + 1, j, visited, pos + 1) \
+            or self.getWords(board, word, i - 1, j, visited, pos + 1)
+        visited[(i, j)] = False
+
+        return res
+
+
+board = [
+    ['A', 'B', 'C', 'E'],
+    ['S', 'F', 'C', 'S'],
+    ['A', 'D', 'E', 'E']
+]
+
+print(Solution().exist(board, "ABCCED"))
